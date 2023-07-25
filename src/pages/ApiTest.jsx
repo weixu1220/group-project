13104c3fe95ecd108ecfe5dae5f894433328848b
josephcarpinelli <carpinelli.dev@protonmail.ch>
2023-07-25 11:44:56 -0600

    Updates ApiTest to pull the insult from the EvilInsults API.

--- src/pages/ApiTest.jsx
+++ src/pages/ApiTest.jsx
@@ -1,25 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import getBearPicture from "../utility/PlaceBearAPI";
+import getEvilInsult from "../utility/EvilInsultAPI";
 
 
 const ApiTest = function()
 {
     const [imageURL, setImageURL] = useState("");
+    const [insult, setInsult] = useState("");
+
     const getImageURL = async function(width, height)
     {
         const nextURL = await getBearPicture(width, height);
         setImageURL((_) => nextURL);
     }
+
+    const getInsult = async function()
+    {
+        const nextInsult = await getEvilInsult();
+        setInsult((_) => nextInsult);
+    }
+
     useEffect(() =>
     {
         getImageURL(300, 500);
+        getInsult();
     }, []);
 
     return (
         <>
             <div>PlaceBear</div>
             <img src={imageURL} alt="" />;
+            <div>EvilInsult</div>
+            <p>{insult}</p>
         </>
     );
 };
