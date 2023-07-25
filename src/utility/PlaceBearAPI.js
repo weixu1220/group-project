a8b5fc395a61547656124c966bd9bf3e414fed20
josephcarpinelli <carpinelli.dev@protonmail.ch>
2023-07-25 11:15:19 -0600

    Updates API call to use URL instead of JSON object.

--- src/utility/PlaceBearAPI.js
+++ src/utility/PlaceBearAPI.js
@@ -1,11 +1,25 @@
 const getBearPicture = async function(width, height)
 {
     const BASE_URL = "https://placebear.com";
-    const requestURL = `${BASE_URL}/${width}/${height}/`;
-    const httpResponse = await fetch(requestURL);
-    const bearPicture = await httpResponse.json;
+    const requestURL = `${BASE_URL}/${width}/${height}`;
+    let bearPicture = null;
+    try
+    {
+        const httpResponse = await fetch(requestURL);
+        const bearPicture = await httpResponse.json();
+        return bearPicture;
+    }
+    catch (exception)
+    {
+        console.log(`Exception: ${exception}`);
+        bearPicture = requestURL;
+    }
+    finally
+    {
+        console.log(`Returning: ${bearPicture}`)
+        return bearPicture;
+    }
 
-    return bearPicture;
 };
 
 
